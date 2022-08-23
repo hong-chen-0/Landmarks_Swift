b98fb8dc715a0d40431436d8921af21211166728
CC <79303996+hong-chen-0@users.noreply.github.com>
2022-08-23 17:40:29 +0900

    123

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 洪辰 on 2022/8/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
