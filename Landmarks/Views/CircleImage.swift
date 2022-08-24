161430e3f2204d70e4efb994e6b82d9d454d7df2
CC <79303996+hong-chen-0@users.noreply.github.com>
2022-08-24 11:31:15 +0900

    123

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image:Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(Circle())
-            .shadow(radius: 7)
             .overlay(
                 Circle().stroke(Color.white, lineWidth: 4)
             )
+            .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image:Image("turtlerock"))
     }
 }
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,37 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height:300)
-            
-            CircleImage()
-                .offset(y:-130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment:.leading) {
-                Text("HAYA")
-                    .font(.title)
-
-                HStack {
-                    Text("great delivery")
-                    Spacer()
-                    Text("fast")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About the HAYA")
-                    .font(.title2)
-                Text("Descriptive")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
